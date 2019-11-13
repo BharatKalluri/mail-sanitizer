82e824f5486c54f0eeef3dbb8aba3f3e57963666
Bharat Kalluri <bharatkalluri@protonmail.com>
2019-11-13 08:21:24 +0530

    Added un subscribe links support

--- cli/mail_dump_ops.py
+++ cli/mail_dump_ops.py
@@ -26,29 +26,41 @@ def dump_mail_data(file_name, obj, overwrite=False):
     dump.close()
 
 
+def _sanitize_un_sub_link(link):
+    return str(link).strip().replace("\u003c", "").replace("\u003e", "")
+
+
 class MailDumpOps:
     def __init__(self):
         self.df = pd.DataFrame(pd.read_pickle(get_mail_dump_path())).transpose()
         self.df['from_sender'] = np.nan
         self.df['from_sender'] = self.df.apply(MailDumpOps.get_from_sender, axis=1)
-        # TODO: get un-subscribe link here
+        self.df['un_subscribe_links'] = self.df.apply(MailDumpOps.get_unsubscribe_links, axis=1)
 
     @staticmethod
     def create_mail_dump(q: str, user_id, mail_client):
-        print("Getting all your emails metadata, this might take a while based on the size of your inbox. "
-              "Go grab a coffee :)")
         all_messages = mail_client.get_messages_for_q(q, user_id)
         print("Getting all your emails contents")
         messages_content = asyncio.run(get_all_emails(user_id, all_messages, mail_client))
         dump_mail_data(get_mail_dump_path(), messages_content, True)
-        print("Collected all emails, run sanitize to clean up your inbox!")
 
     @staticmethod
     def get_from_sender(row):
+        return MailDumpOps.get_header_val(row, 'From')
+
+    @staticmethod
+    def get_unsubscribe_links(row):
+        unsubscribe_links_raw = MailDumpOps.get_header_val(row, 'List-Unsubscribe')
+        if unsubscribe_links_raw:
+            unsubscribe_links = [_sanitize_un_sub_link(link) for link in unsubscribe_links_raw.split(",")]
+            return unsubscribe_links
+
+    @staticmethod
+    def get_header_val(row, header_key):
         payload = row.payload
         if payload and isinstance(payload, dict):
             headers = payload.get('headers')
-            filtered_from_sender = list(filter(lambda x: x['name'] == 'From', headers))
+            filtered_from_sender = list(filter(lambda x: x['name'] == header_key, headers))
             if filtered_from_sender:
                 return filtered_from_sender[0]['value']
 
@@ -57,3 +69,9 @@ class MailDumpOps:
 
     def get_message_ids(self, sender: str):
         return list(self.df[self.df.from_sender == sender].id)
+
+    def get_un_sub_link(self, sender):
+        un_sub_links = list(self.df[self.df.from_sender == sender]['un_subscribe_links'])
+        filtered_un_sub_links = list(filter(lambda x: x is not None, un_sub_links))
+        if filtered_un_sub_links and any(filtered_un_sub_links):
+            return " , ".join([str(el) for el in filtered_un_sub_links])
--- cli/main.py
+++ cli/main.py
@@ -14,18 +14,22 @@ def cli():
 
 @cli.command()
 def sanitize():
+    # TODO: Notify if mail dump does not exist, ask to run collect
     user_email = get_prop_from_config("email")
     mail_client = GmailClient()
     mail_dump_ops = MailDumpOps()
     top_senders = mail_dump_ops.get_top_senders()
     for sender in top_senders:
         emails_ids = mail_dump_ops.get_message_ids(sender)
-        print(f"You have {len(emails_ids)} messages from {sender}, "
-              f"do you want to delete all messages from these senders? (y/n) ", end="")
+        un_sub_links = mail_dump_ops.get_un_sub_link(sender)
+        print(f"You have {len(emails_ids)} messages from {sender}, Delete? (y/n) ", end="")
         should_del = str(input())
         if should_del and should_del[0] == 'y':
             mail_client.del_emails_with_id(emails_ids, user_email)
             print(f"Deleted {len(emails_ids)} emails!")
+        # TODO: Better format cli and educate people about how mailto for un sub works
+        if un_sub_links:
+            print(f"Unsubscribe links: {un_sub_links}")
 
 
 @cli.command()
@@ -33,7 +37,10 @@ def collect():
     # Create a mail dump with everything in existence
     gmail_client = GmailClient()
     user_email = get_prop_from_config("email")
+    print("Getting all your emails metadata, this might take a while based on the size of your inbox. "
+          "Go grab a coffee :)")
     MailDumpOps.create_mail_dump("", user_email, gmail_client)
+    print("Collected all emails, run sanitize to clean up your inbox!")
 
 
 def main():
