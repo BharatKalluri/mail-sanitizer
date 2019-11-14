052fbffa5ad166d99cece593e0adde408a096ad9
Bharat Kalluri <bharatkalluri@protonmail.com>
2019-11-15 01:23:43 +0530

    Using find packages

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
     name='mail-sanitizer',
@@ -6,7 +6,7 @@ setup(
     license="GPLv3",
     description="A cli tool to clean up your email",
     url="https://github.com/BharatKalluri/mail-sanitizer",
-    packages=['mail_sanitizer'],
+    packages=find_packages(),
     install_requires=["PyYAML", "aiohttp", "pandas", "requests", "numpy", "google-auth", "click",
                       "google-auth-oauthlib"],
     entry_points={
